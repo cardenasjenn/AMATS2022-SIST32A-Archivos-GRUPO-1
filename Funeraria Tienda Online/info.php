bd10198e44ebffec5dc5f72ce1fd0d12f3ce2c06
cardenasjenn <jenniffer.cardenas21@itca.edu.sv>
2022-06-01 22:17:35 -0600

    Agregar información de la funeraria

--- Funeraria Tienda Online/info.php
+++ Funeraria Tienda Online/info.php
@@ -26,12 +26,124 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.php">Funeraria la unica esperanza</a>
+      <a class="navbar-brand" href="index.php">Funeraria la única esperanza</a>
     </div>
   </div>
 </nav>
 
 <div class="container" id="main">
+<table align="center" width="85%">
+
+<tr>
+<td colspan="2" align="left">
+<br><br>
+<dl>
+<dd><h4 align="center"> La Funeraria "La Única Esperanza" fue fundada en el año 1998 en Sonsonate por Carlos Reyes,<br> tenemos 22 años en la industria de servicios funerales proporcionando a nuestros clientes los servicios<br> necesarios de la mejor manera y con la mejor actitud posible, a lo largo de los años hemos ido actualizando y<br> agregando más servicios con el único propósito de que los clientes encuentren el producto adecuado.    </h4><dd>
+<dd><h4 align="center"> Luego en 2002 la Funeraria se expandió a Santa Ana, implementando así el local de dicho departamento,<br> el cual de igual manera fue fundado por Carlos Reyes. </h4><dd>
+<br>
+<tr>
+<td align="left">
+<dd><h3 align="center">Misi&oacute;n</h3></dd>
+
+  <dd><h4 style="font-style: italic;">&#34; Brindar el mejor servicio hacia nuestros clientes demostrando así, el compromiso con la sociedad, ofreciéndoles de la manera más amable y humana asistencia en los momentos difíciles velando por la tranquilidad de quienes deposita la confianza en nosotros.&#34;</h4></dd>
+  
+<td align="left">
+<dl>
+<dd><h3 align="center">Visi&oacute;n</h3></dd>
+
+  <dd><h4 style="font-style: italic;">&#34; Ser la empresa líder de servicios funerarios en El Salvador, logrando satisfacer las necesidades de nuestros clientes, para lograr así un mayor prestigio para la compañía y de esta forma poder, a nivel nacional, brindar nuestros servicios con la mayor calidad y amabilidad posible.&#34;</h4></dd>
+
+</td>
+</tr>
+
+
+<td colspan="2" align="left">
+  <br><br>
+  <dd><h4> Somos una empresa dedicada a brindar servcios funerales a nuestros clientes, tenemos varios tipos de servcios buscando asi darle al cliente la oportunidad<br> de obtener lo que mejor se acople a lo que est&aacute; buscando. </h4><dd>
+
+<dd><h4 align="left">Nuestros locales se encuentran ubicados en: </h4></dd>
+</dl>
+</td>
+</tr>
+
+<tr>
+<td align="left">
+<dl>
+  <dd><h4><img src="imagenes/icono1.png" width="1.8%" align="center"> Santa Ana</h4></dd>
+</dl>
+</td>
+<td align="left" >
+<h4><img src="imagenes/icono1.png" width="2%" align="center"> Sonsonate</h4>
+</td>
+</tr>
+
+<tr>
+<td>
+  <img src="imagenes/local1.png" width="90%" height="400px">
+
+  <br><br><br>
+</td>
+<td>
+  <img src="imagenes/local2.jpg" width="100%" height="400px">
+  
+  <br><br><br>
+</td>
+</tr>
+
+
+</table>
+
+<br>
+<br>
+
+
+
+</section>
+
+
+<footer id="pie"> 
+<table style="background-color: #111f4d" width="100%">
+<tr>
+  <td width="40%" colspan="2" valign="top">
+    <h2 style="color: #FFFFFF;"> Horario de Atención</h2>
+    <p  style="color: #FFFFFF"> Nuestros locales estan abiertos y disponibles para ti 24/7, en el momento que <br>necesites nuestros servicios puedes llamar o cosultar a trav&eacute;s de:</p>
+  </td>
+  <td valign="top" style="text-align: left;" width="16.5%" colspan="2" rowspan="2">
+    <h2 align="left" style="color: #FFFFFF">Enlaces</h2>
+    <a  style="width: 40%; background-color:#111f4d; color: #FFFFFF;text-align: right;" href="nuestrosservicios.html"><u>  Nuestros Servicios</u></a><br>
+    <a style="width: 40%; background-color:#111f4d; color: #FFFFFF;text-align: right;" href="quienessomos.html"><u>&iquest;  Quienes Somos?</u></a><br>
+    <a style="width: 40%; background-color:#111f4d; color: #FFFFFF; text-align: center;" href="contactanos.html"><u>  Contactanos</u></a> 
+
+  
+  </td>
+  <td valign="top" width="30%">
+    <h2 style="color: #FFFFFF">Visítanos</h2>
+    <p style="color: #FFFFFF">Estamos ubicados en:</p>
+
+  </td>
+</tr>
+<tr>
+  <td valign="top" colspan="2">
+    <a style="text-align: center;" target="_blank" href="https://www.facebook.com/funeraleslaunicaesperanza"><img src="imagenes/facebook.png" width="9%" align="center" hspace="5px">Funerales La &Uacute;nica Esperanza</a>
+
+
+    <a style="text-align: center;"  href="tel:+50361189645"><img src="imagenes/whatsapp.png" width="10%" align="center" hspace="5px">6118-9645</a>
+
+    <a style="text-align: center;" target="_blank" href="mailto:unicaesperanza@gmail.com"><img src="imagenes/gmail.png" width="10%" align="center" hspace="5px"> unicaesperanza@gmail.com</a>
+
+
+    <a style="text-align: center;" href="tel:+50324411926"><img src="imagenes/whatsapp.png" width="10%" align="center" hspace="5px">2441-1926</a>
+  
+
+  </td>
+  <td>
+    <p style="color: #FFFFFF" >6ta Av. Norte (entre 6ta y 8va), Calle poniente, Barrio San Juan, Santa Ana.</p>
+  </td>
+  
+</tr>
+
+</table>
+
 
     
 
